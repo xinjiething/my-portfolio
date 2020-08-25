8f1c940dcf198502c1bd9c10120358ef5261f480
Xin Jie Thing <thingxinjie@gmail.com>
2020-08-25 23:33:36 +0800

    responsive

--- javascript/accordion.js
+++ javascript/accordion.js
@@ -1,5 +1,7 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
+var acc2 = document.getElementsByClassName("accordion2");
+var j;
 
 for (i = 0; i < acc.length; i++) {
     // console.log(acc[i]);
@@ -8,6 +10,28 @@ for (i = 0; i < acc.length; i++) {
         this.nextElementSibling.classList.toggle("show");
     }
 };
+
+// for (j = 0; j < acc2.length; j++){
+//     acc[j].onclick = function(){
+//         for (i = 0; i < acc.length; i++) {
+//             // console.log(acc[i]);
+//             // acc[i].onclick = function(){
+//             this.classList.toggle("active");
+//             this.nextElementSibling.classList.toggle("show");
+//             // }
+//         };
+//         this.classList.toggle("active");
+//         this.nextElementSibling.classList.toggle("show");
+//     }
+// }
+
+// for (i = 0, j=0; i < acc.length && j < acc2.length; i++, j++) {
+//     // console.log(acc[i]);
+//     acc[i].onclick = function(){
+//         this.classList.toggle("active");
+//         this.nextElementSibling.classList.toggle("show");
+//     }
+// };
 
 
 function openNav() {
